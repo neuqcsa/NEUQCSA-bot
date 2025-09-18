a59fd00594b9e577dc30f1086e1150e9527aabea
sygiegiehs <124494789+sygiegiehs@users.noreply.github.com>
2025-09-18 09:39:14 +0800

    Add: messages for ?CTF Opt: update address keywords

--- plugins/nonebot_plugin_neuqcsa/reply.py
+++ plugins/nonebot_plugin_neuqcsa/reply.py
@@ -4,8 +4,7 @@ from .utils import config, resource
 
 map = on_fullmatch(("地图"))
 calendar = on_fullmatch(("校历"))
-address = on_fullmatch(("快递地址"))
-register = on_fullmatch(("报到", "报到时间"))
+address = on_fullmatch(("快递地址", "邮寄地址", "学校地址", "邮编"))
 train = on_fullmatch(("军训", "军训时间"))
 poster = on_fullmatch(("网安海报"))
 
@@ -18,15 +17,9 @@ async def handle_calendar():
 @address.handle()
 async def handle_address():
     await address.finish("河北省秦皇岛市海港区白塔岭街道泰山路143号东北大学秦皇岛分校工学馆东侧\n邮编：066004")
-@register.handle()
-async def handle_register():
-    await register.finish("新生报到时间：8月30日至31日")
 @train.handle()
 async def handle_train():
     await train.finish("学校将按照国家有关规定开展新生军训，军训时间为2025年9月14日至27日，共14天。")
 @poster.handle()
-async def handle_poster(event: GroupMessageEvent): # 由于需要判断群号，该功能在私聊无效
-    if event.group_id in config.no_qrcode_group_ids:
-        await poster.finish(resource("2025招新海报-无二维码.png") + "群号：" + str(config.neuqcsa_group_id))
-    else:
-        await poster.finish(resource("2025招新海报.png"))
+async def handle_poster():
+    await poster.finish(resource("？CTF海报.png") + "群号：" + str(config.neuqcsa_group_id))
--- plugins/nonebot_plugin_neuqcsa/schedule.py
+++ plugins/nonebot_plugin_neuqcsa/schedule.py
@@ -9,10 +9,7 @@ async def send_poster():
     logger.info("Sending poster...")
     bot = nonebot.get_bot()
     for schedule_group_id in config.schedule_group_ids:
-        if schedule_group_id in config.no_qrcode_group_ids:
-            poster_path = "2025招新海报-无二维码.png"
-        else:
-            poster_path = "2025招新海报.png"
+        poster_path = "？CTF海报.png"
         await bot.send_group_msg(group_id=schedule_group_id, message="🎉网络空间安全协会招新开始啦🎉\n"
                                                                      "😎加入我们，成为黑客！😎\n"
                                                                      "💪从网站攻击到算法破解应有尽有💪\n"
@@ -20,11 +17,17 @@ async def send_poster():
                                                                      "🕹️从竞赛奖项到科创加分拿到手软🕹️\n"
                                                                      "\n"
                                                                      "🎭参加黑客夏令营，解开黑客攻防的秘密🎭\n"
-                                                                     "😋2025暑期课程已开始，学长学姐带你从零入门😋\n"
+                                                                     "😋本轮招新计划将在9.27开始，在 ?CTF 的赛场上展现你的锋芒😋\n"
                                                                      "👉加群开启黑客之旅：" + str(config.neuqcsa_group_id) + "\n"
                                                                      "不管你是零基础还是大神都可以加入进来！详细信息可以到协会官网了解：www点neuqcsa点cn" + resource(poster_path))
+<<<<<<< HEAD
+        # for superuser in config.superusers:
+        #     await bot.send_private_msg(user_id=superuser, message=str(datetime.now()) + " 已向群 " + str(schedule_group_id) + " 发送 " + poster_path)
+=======
         for superuser in config.superusers:
-            await bot.send_private_msg(user_id=superuser, message=str(datetime.now()) + " 已向群 " + str(schedule_group_id) + " 发送 " + poster_path)
+            await bot.send_private_msg(user_id=superuser, message=str(datetime.now()) + " 已向群 " + str(
+            schedule_group_id) + " 发送 " + poster_path)
+>>>>>>> parent of 0a8c2e5 (Chore: reformat code)
 
 # 定时任务，基于装饰器的方式
 # @scheduler.scheduled_job("cron", hour=8, id="job_8", misfire_grace_time=None)
